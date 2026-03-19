f01a855be6e879409dda7174b0b0dc46e9ae4eb4
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-03-19 07:17:39 -0500

    Made User object builder pattern

--- src/main/java/com/noobsmoke/basedblogbackend/model/User.java
+++ src/main/java/com/noobsmoke/basedblogbackend/model/User.java
@@ -16,9 +16,8 @@ import java.util.List;
 @Getter
 @Setter
 @ToString
-@AllArgsConstructor
 public class User {
-    private long id;
+    private Long id;
     private String firstName;
     private String lastName;
     private String userName;
@@ -26,5 +25,75 @@ public class User {
     private LocalDate createdDate;
     private String avatar;
     private List<String> favorite_topics;
+
+    private User(Builder builder) {
+        this.id = builder.id;
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.userName = builder.userName;
+        this.password = builder.password;
+        this.createdDate = builder.createdDate;
+        this.avatar = builder.avatar;
+        this.favorite_topics = builder.favorite_topics;
+    }
+
+    public static class Builder {
+        private Long id;
+        private String lastName;
+        private String firstName;
+        private String userName;
+        private String password;
+        private LocalDate createdDate;
+        private String avatar;
+        private List<String> favorite_topics;
+
+        public Builder() {
+
+        }
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder firstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder userName(String userName) {
+            this.userName = userName;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder createdAt(LocalDate createdDate) {
+            this.createdDate = createdDate;
+            return this;
+        }
+
+        public Builder avatar(String avatar) {
+            this.avatar = avatar;
+            return this;
+        }
+
+        public Builder favoriteTopics(List<String> favorite_topics) {
+            this.favorite_topics = favorite_topics;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
 }
 
