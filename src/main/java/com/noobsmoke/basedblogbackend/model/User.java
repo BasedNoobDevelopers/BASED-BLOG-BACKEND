8b419aafe5b90db1260355c440d508687f014cab
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-03-19 15:05:28 -0500

    Worked on JWT Service class

--- src/main/java/com/noobsmoke/basedblogbackend/model/User.java
+++ src/main/java/com/noobsmoke/basedblogbackend/model/User.java
@@ -5,9 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 
@@ -15,11 +18,11 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @ToString(exclude = "password")
-public class User {
+public class User implements UserDetails {
     private Long id;
     private String firstName;
     private String lastName;
-    private String userName;
+    private String username;
     private String password;
     private String email;
     private LocalDateTime createdDate;
@@ -30,12 +33,38 @@ public class User {
         this.id = builder.id;
         this.firstName = builder.firstName;
         this.lastName = builder.lastName;
-        this.userName = builder.userName;
+        this.username = builder.userName;
         this.password = builder.password;
         this.email = builder.email;
         this.createdDate = builder.createdDate;
         this.avatar = builder.avatar;
         this.favoriteTopics = builder.favoriteTopics;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 
     public static class Builder {
---
+++ src/main/java/com/noobsmoke/basedblogbackend/service/JWTService.java
@@ -0,0 +1,78 @@
+package com.noobsmoke.basedblogbackend.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JWTService {
+
+    @Value("${security.jwt.secret-key}")
+    private String secretKey; //Secret Key for JWT signing
+
+    @Getter
+    @Value("${security.jwt.expiration-time}")
+    private long jwtExpirationTime;
+
+    // This function extracts a username from the JWT Token
+    public String extractUsername(String jwtToken) {
+        return extractClaim(jwtToken, Claims::getSubject);
+    }
+
+    // This function extracts a specific claim from the JWT Token
+    public <T> T extractClaim(String jwtToken, Function<Claims, T> claimsResolve) {
+        final Claims claims = extractAllClaims(jwtToken);
+        return claimsResolve.apply(claims);
+    }
+
+    // This function builds a JWT Token with additional claims from the given user details
+    private String buildToken(Map<String, Object> extraClaims, UserDetails userDetails, long expiration) {
+        return Jwts.builder().claims(extraClaims).subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    // This function makes the signing key for the JWT token generation
+    private Key getSignInKey() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+    }
+
+    // Check if the JWT Token is valid for the given user details
+    public boolean isTokenValid(String jwtToken, UserDetails userDetails) {
+        final String usernameFromToken = extractUsername(jwtToken);
+        return (usernameFromToken.equals(userDetails.getUsername())) && !isTokenExpired(jwtToken);
+    }
+
+    // This function checks if JWT Token is expired
+    private boolean isTokenExpired(String jwtToken) {
+        return extractExpiration(jwtToken).before(new Date());
+    }
+
+    // This function extracts the expiration date from the JWT Token
+    private Date extractExpiration(String jwtToken) {
+        return extractClaim(jwtToken, Claims::getExpiration);
+    }
+
+    // This function extracts all the claims from the JWT Token
+    private Claims extractAllClaims(String jwtToken) {
+        return Jwts.parser()
+                .verifyWith((SecretKey) getSignInKey())
+                .build()
+                .parseSignedClaims(jwtToken)
+                .getPayload();
+
+    }
+}
