15e35c5b407209e9c72179fa0de75d12a68c1c22
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-03-26 20:25:22 -0500

    Removed public identifier in the class name

--- src/test/java/com/noobsmoke/basedblogbackend/unit/service/UserServiceTest.java
+++ src/test/java/com/noobsmoke/basedblogbackend/unit/service/UserServiceTest.java
@@ -21,7 +21,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class UserServiceTest {
+class UserServiceTest {
 
     @Mock
     private FakeRepo repo;
