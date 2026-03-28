a322e32367ea380ca2eaa9e7157819befc6a5c85
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-03-28 09:14:14 -0500

    Worked on Email Verification Service class

---
+++ src/main/java/com/noobsmoke/basedblogbackend/service/EmailVerificationService.java
@@ -0,0 +1,31 @@
+package com.noobsmoke.basedblogbackend.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.AllArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmailVerificationService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendVerificationEmail(String to, String subject, String text) {
+        try {
+            MimeMessage emailMessage = mailSender.createMimeMessage();
+            MimeMessageHelper emailMessageHelper = new MimeMessageHelper(emailMessage, true);
+
+            emailMessageHelper.setTo(to);
+            emailMessageHelper.setSubject(subject);
+            emailMessageHelper.setText(text, true);
+
+            mailSender.send(emailMessage);
+        } catch (MessagingException e) {
+            throw new RuntimeException("Email Not Sent");
+        }
+
+    }
+}
